6c06b91d40000b18e6e42c7454d726e5a92f6b2f
iespenanovo <correo.iaw@gmail.com>
2020-10-22 11:03:18 +0200

    traballo clase iaw

--- php/07-calendario.php
+++ php/07-calendario.php
@@ -6,9 +6,16 @@
 	<title>Calendario</title>
 	<style>	
 		#mes {
+			max-width: 350px;
+			width: 70%;
+			margin: 0 auto;
+
 			text-align: center;
 			font-weight: bold;
 			margin-bottom: 15px;
+			display: flex;
+			justify-content: space-between;
+
 
 		}
 		#calendario {
@@ -42,24 +49,39 @@
 	$diaActual=intval(strftime("%e"));//intval para pasar a enteiro
 	$mesActual=intval(strftime("%m"));
 	$anoActual=intval(strftime("%Y"));
-	$nomeMesActual=strftime("%B");
+	
+
+	$mes=$_GET["mes"]??$mesActual;
+	$mes=intval($mes);	
+	$ano=$_GET["ano"]??$anoActual;
+	$ano=intval($ano);
+
+	if($mes<1 || $mes>12) $mes=$mesActual;
+
+	$nomeMes=strftime("%B",mktime(0,0,0,$mes,1,$ano));
+	//echo "<p>mes a facer: $mes / $ano</p>";
 
 	//echo "<br>diaActual: $diaActual, mesActual: $mesActual, anoActual: $anoActual";
 
-	if(checkdate(2, 29, $anoActual)){//si se cumple é ano bisesto
+	if(checkdate(2, 29, $ano)){//si se cumple é ano bisesto
 		$diasMes[2]=29;
 	}
 
 	//echo "<br>Este mes ($mesActual) ten $diasMes[$mesActual] dias";
 
-	$diaSemDia1=strftime("%u",mktime(0,0,0,$mesActual,1,$anoActual));
+	$diaSemDia1=strftime("%u",mktime(0,0,0,$mes,1,$ano));
 	$diaSemDia1=intval($diaSemDia1);
 	//echo "<br>O dia 1 de este mes é o día1 $diaSemDia1 da semana";
 
 ?>
 
 	<div id="mes">
-		<?php echo "$nomeMesActual - $anoActual" ?>
+
+		<?php
+			echo "<div><a href='?mes=9&ano=2020'>Ant.</a></div>";
+		 	echo "<div>$nomeMes - $ano</div>";
+			echo "<div><a href='?mes=11&ano=2020'>Seg.</a></div>";
+		?>
 	</div>
 	<div id="calendario">
 			<div class="cab">Lun</div>	
@@ -74,8 +96,8 @@
 			echo "\n\t\t\t<div></div>";
 		}
 
-		for ($i=1; $i <= $diasMes[$mesActual]; $i++) { 
-			$clase=$i==$diaActual?"diaActual":"";
+		for ($i=1; $i <= $diasMes[$mes]; $i++) { 
+			$clase=($i==$diaActual && $mes==$mesActual && $ano==$anoActual)?"diaActual":"";
 			echo "\n\t\t\t<div class='$clase'>$i</div>";
 		}
 
