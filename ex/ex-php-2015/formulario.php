245b59bac9f73da9f39af92cead2bda7eed3c7fd
iespenanovo <correo.iaw@gmail.com>
2020-12-01 14:16:08 +0100

    traballo clase iaw

--- ex/ex-php-2015/formulario.php
+++ ex/ex-php-2015/formulario.php
@@ -6,85 +6,113 @@
   <link rel="stylesheet" href="css/formulario.css">
 </head>
 <body>
-<h1 class="nombre">Pon aquí tu nombre y apellidos</h1>
-<div id="formulario">
-  <form action="formulario.php" method="post">
-    <h2>Pedido de Pizza</h2>
+  <h1 class="nombre">Pon aquí tu nombre y apellidos</h1>
+  <div id="formulario">
+    <form action="formulario.php" method="post">
+      <h2>Pedido de Pizza</h2>
 
-    <fieldset>
-      <legend>Datos envío</legend>
-      <div class="datose">
+      <fieldset>
+        <legend>Datos envío</legend>
+        <div class="datose">
           <label for="nom">Nombre:</label>
           <input type="Text" name="nom" size="40" maxlength="30" id="nom">
-      </div>
+        </div>
 
-      <div class="datose">
+        <div class="datose">
           <label for="dir">Dirección:</label>
-        <input type="text" name="dir" size="50" maxlength="30" id="dir">
-      </div>  
-    </fieldset>
+          <input type="text" name="dir" size="50" maxlength="30" id="dir">
+        </div>  
+      </fieldset>
 
-    <fieldset>
-      <legend>Detalles pedido</legend>
+      <fieldset>
+        <legend>Detalles pedido</legend>
 
-      <div class="detallesp">
+        <div class="detallesp">
           <label class="rotulo">Ingredientes</label>
           <label for="queso"> Queso</label>
-        <input type="CheckBox" name="ingre[]" value="Q" id="queso"><br>
+          <input type="CheckBox" name="ingre[]" value="Q" id="queso"><br>
           <label for="pimiento"> Pimiento</label>
-        <input type="CheckBox" name="ingre[]" value="P" id="pimiento"><br>
+          <input type="CheckBox" name="ingre[]" value="P" id="pimiento"><br>
           <label for="cebolla"> Cebolla</label>
-        <input type="CheckBox" name="ingre[]" value="C" id="cebolla"><br>
-      </div>
+          <input type="CheckBox" name="ingre[]" value="C" id="cebolla"><br>
+        </div>
 
-      <div class="detallesp">
-        <label class="rotulo">Tamaño</label>
+        <div class="detallesp">
+          <label class="rotulo">Tamaño</label>
           <label for="peque"> Pequeña</label>
-        <input type="Radio" name="tam" value="10" id="peque"><br>
+          <input type="Radio" name="tam" value="10" id="peque"><br>
           <label for="med"> Mediana</label>
-        <input type="Radio" name="tam" value="12" id="med"><br>
+          <input type="Radio" name="tam" value="12" id="med"><br>
           <label for="grand"> Grande</label>
-        <input type="Radio" name="tam" value="16" id="grand"><br>
-      </div>
+          <input type="Radio" name="tam" value="16" id="grand"><br>
+        </div>
 
-      <div class="detallesp">
-        <label for="ins">Instrucciones Especiales</label>
-        <textarea name="ins" rows="5" id="ins" placeholder="Escribe aquí"></textarea>       
-      </div>
+        <div class="detallesp">
+          <label for="ins">Instrucciones Especiales</label>
+          <textarea name="ins" rows="5" id="ins" placeholder="Escribe aquí"></textarea>       
+        </div>
 
-    </fieldset>
+      </fieldset>
 
-    <fieldset>
-      <legend>Forma de Pago</legend>
+      <fieldset>
+        <legend>Forma de Pago</legend>
 
-      <div class="datosp">
-        <label for="fpago">Datos Pago:</label>
-        <select name="fpago" size="1" id="fpago">
-          <option value=""></option>
-          <option value="1">Contado</option>
-          <option value="2">Visa</option>
-          <option value="3">Cheque</option>
-        </select>
-      </div>
+        <div class="datosp">
+          <label for="fpago">Datos Pago:</label>
+          <select name="fpago" size="1" id="fpago">
+            <option value=""></option>
+            <option value="1">Contado</option>
+            <option value="2">Visa</option>
+            <option value="3">Cheque</option>
+          </select>
+        </div>
         
-    </fieldset>
+      </fieldset>
 
-    <p class="botones">
-      <input type="submit" name="enviar" value="Enviar" >
+      <p class="botones">
+        <input type="submit" name="enviar" value="Enviar" >
         <input type="reset" value="Borrar">
-    </p>
+      </p>
 
-  </form>
+    </form>
 
-  <?php 
-  $nom=isset($_POST["nom"])?$_POST["nom"]:"";
-  $dir=isset($_POST["dir"])?$_POST["dir"]:"";
-  $ingre=isset($_POST["ingre"])?$_POST["ingre"]:"";
-  $tam=isset($_POST["tam"])?$_POST["tam"]:"";
-  $fpago=isset($_POST["fpago"])?$_POST["fpago"]:"";
-  $ins=isset($_POST["ins"])?$_POST["ins"]:"";
+    <?php 
+    $nom=isset($_POST["nom"])?$_POST["nom"]:"";
+    $dir=isset($_POST["dir"])?$_POST["dir"]:"";
+    $ingre=isset($_POST["ingre"])?$_POST["ingre"]:array();
+    $tam=isset($_POST["tam"])?$_POST["tam"]:"";
+    $fpago=isset($_POST["fpago"])?$_POST["fpago"]:"";
+    $ins=isset($_POST["ins"])?$_POST["ins"]:"";
+    $enviado=isset($_POST["enviar"])?true:false;
 
-  $enviado=isset($_POST["enviar"])?true:false;
+
+    $faltanCampos=0; 
+    if ($_POST) {
+          if($nom=="") $faltanCampos++;
+          if($dir=="") $faltanCampos++;
+          if(count($ingre)<1) $faltanCampos++;
+          if($tam=="") $faltanCampos++;
+          if($fpago=="") $faltanCampos++;
+          if($ins=="") $faltanCampos++;
+
+          if($faltanCampos>0) {
+            echo "<p class='noaceptado'>Faltan campos por cubrir</p>";
+
+          }
+          else {
+            echo "<p class='aceptado'>Formulario aceptado</p>";
+          $ficheiro=@fopen("formulario.txt", "a") or die("<p>Non foi posible abrir o ficheiro datos.csv</p>"); 
+          $depIngre=implode("", $ingre);
+          fputs($ficheiro,"$nom;$dir;$depIngre;$tam;$fpago;$ins\n");
+          fclose($ficheiro);
+
+          }
+
+    }
+
+
+
+
 
   /* 
   Introducir aquí código php para grabar los datos del formulario en el archivo formulario.txt.
