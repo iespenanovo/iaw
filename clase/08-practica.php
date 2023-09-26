61d1283be2b842c7078764c87071eb42274f5b17
iespenanovo <correo.iaw@gmail.com>
2023-09-26 09:35:19 +0200

    repositorio IAW

--- clase/08-practica.php
+++ clase/08-practica.php
@@ -10,18 +10,40 @@
 	<div class="contenedor">
 		
 		<h1>Genera números del 1 al x</h1>
-<?php 
-	$hasta=$_GET["hasta"]??"";
-?>
+		<?php 
+			$hasta=$_GET["hasta"]??"";
+		?>
 		<form action="">
 			<div class="campo">
-				<label for="hasta">Número final:</label>
+				<label for="hasta">Número X:</label>
 				<input id="hasta" type="number" name="hasta" required value="<?php echo $hasta ?>" >
 			</div>
 			<div class="campo">
 				<input type="submit" value="Enviar">
 			</div>
 		</form>
+
+		<?php 
+		if ($_GET) { //se cumple solo si el formulario fue enviado (la primera vez no se cumple)
+			echo "\n<h3>Números del 1 al $hasta:</h3>";	
+			echo "\n<p class='numeros'>";
+			$color="rojo";
+			for ($numero=1; $numero <= $hasta ; $numero++) { 
+				echo "<span class='$color'>$numero</span>, ";
+				// if ($color=="azul") {
+				// 	$color="rojo";
+				// } else {
+				// 	$color="azul";
+				// }
+				//el if else anterior equivale a:
+				$color = $color=="azul" ? "rojo" : "azul";
+				
+			}
+			echo "</p>";
+		}
+		
+
+		?>
 
 	</div>
 
