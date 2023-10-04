0c934593bb30371c3c088be27e20ac68f8d5a641
iespenanovo <correo.iaw@gmail.com>
2023-10-04 11:15:07 +0200

    repositorio IAW

--- clase/14-ficheros-leer.php
+++ clase/14-ficheros-leer.php
@@ -4,10 +4,13 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Ficheros</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
-	<h1>Leer fichero csv desde PHP</h1>
-
+	<div class="container">
+	<h1 class="text-center">Leer fichero csv desde PHP</h1>
+	<table class="table table-dark table-striped">
+		
 <?php 
 	$fichero="doc/datos1.csv";
 	//abrimos fichero para solo lectura, controlando posible error 
@@ -21,7 +24,14 @@
 		$campos=explode(";", $linea);
 		list($nombre,$edad,$dni,$poblacion)=$campos;
 		//echo "<p>$campos[0] - $campos[1] - $campos[2] - $campos[3] </p>";
-		echo "<p>$nombre - $edad - $dni - $poblacion</p>";
+
+		echo "\n<tr>";
+		echo "\n\t<td>$nRegistros</td>";
+		echo "\n\t<td>$nombre</td>";
+		echo "\n\t<td>$edad</td>";
+		echo "\n\t<td>$dni</td>";
+		echo "\n\t<td>$poblacion</td>";
+		echo "\n</tr>";
 
 		$linea=fgets($cursor);
 	}
@@ -29,5 +39,9 @@
 	fclose($cursor);//cerramos fichero
 
 ?>
+	</table>
+	</div>
+	
+
 </body>
 </html>
---
+++ clase/15-ficheros-escribir.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Escritura ficheros PHP</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">	
+</head>
+<body>
+	<div class="container">
+		<h1 class="text-center">Escritura ficheros en PHP</h1>
+		<form>
+			<div class="mb-3">
+				<label for="poblacion" class="form-label">Nombre</label>
+				<input type="text" class="form-control" id="nombre">
+			</div>
+			<div class="mb-3">
+				<label for="edad" class="form-label">edad</label>
+				<input type="number" class="form-control" id="edad">
+			</div>
+			<div class="mb-3">
+				<label for="dni" class="form-label">dni</label>
+				<input type="text" class="form-control" id="dni">
+			</div>
+			<div class="mb-3">
+				<label for="poblacion" class="form-label">poblacion</label>
+				<input type="text" class="form-control" id="poblacion">
+			</div>
+
+			<button type="submit" class="btn btn-primary">Guardar</button>
+		</form>	
+	</div>
+</body>
+</html>
