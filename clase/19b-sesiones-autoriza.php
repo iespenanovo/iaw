e870fac31f915f78e86309b9cd4b0762e63865b9
iespenanovo <correo.iaw@gmail.com>
2023-10-30 14:15:11 +0100

    repositorio IAW

--- clase/19b-sesiones-autoriza.php
+++ clase/19b-sesiones-autoriza.php
@@ -1,1 +1,33 @@
-19b-sesiones-autoriza.php
+<?php
+//
+session_start();
+
+
+//leemos los parámetros del login
+$usuario=$_POST['usuario']??"";
+$clave=$_POST['clave']??"";
+
+
+//definimos las credenciales correctas (lo usual es consultar a la BD)
+// $claveBD=hash('md5', 'Abc123..');
+// echo "<hr>$claveBD<hr>";
+// $claveBD=@crypt('Abc123..');
+// echo "$claveBD<hr>";
+
+
+$usuarioBD="ana";
+$claveBD='$1$DOAFK4wZ$vmD2K6K3WVUkjAUBEfIE.0';
+
+$credenciales=true;
+
+if ($usuario==$usuarioBD) {
+	//el usuario existe
+	if(password_verify(password, hash))
+}
+
+
+
+
+
+
+?>
